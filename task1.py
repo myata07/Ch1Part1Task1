24edd8d563610108187f5ae86fe5e1bd1faa5a22
Home <Home@Homes-MacBook-Pro.local>
2020-03-09 14:04:32 +0600

    first commit

---
+++ task1.py
@@ -0,0 +1,15 @@
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False    
+def test_is_even():
+    assert is_even(2) == True
+    assert is_even(3) == False
+    assert is_even(8) == True
+    assert is_even(100) == True
+    assert is_even(101) == False
+print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_is_even()
+print(is_even(12))
